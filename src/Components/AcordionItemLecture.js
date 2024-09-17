1008d88650ffc8baa192c8d770186823be5b4b54
kemal2709 <k27603009@gmail.com>
2024-09-18 02:25:27 +0300

    first commit

--- src/Components/AcordionItemLecture.js
+++ src/Components/AcordionItemLecture.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import Accordion from 'react-bootstrap/Accordion';
-//import YouTubePlayer from './YouTubePlayer';
+import YouTubePlayer from './YouTubePlayer';
 export default function AcordionItemLecture({eventKey, header}) {
     return (
         <Accordion.Item eventKey={eventKey}>
             <Accordion.Header>{header}</Accordion.Header>
             <Accordion.Body>
-                {/* <YouTubePlayer url={"https://www.youtube.com/watch?v=tDO5LpPMmdU"} /> */}
+                <YouTubePlayer url={"https://www.youtube.com/watch?v=tDO5LpPMmdU"} />
             </Accordion.Body>
         </Accordion.Item>
     )
